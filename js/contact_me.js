6658088f3c3f9ff44ea2d4f7544233ceb89cebc9
jzampino <joseph@blacktrax.ca>
2017-03-01 12:18:15 -0500

    Modifications to Contact form and JS
    Modified Contact form and associated JS file to try and get this
    working.

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,40 +18,43 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+			
             $.ajax({
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSc-ENYyT8tL53oSxCMH8al-h2ObLuhYf45QFrdimuDlCKJN_g/viewform",
-                type: "POST",
+                url: "https://docs.google.com/forms/d/e/1FAIpQLSc-ENYyT8tL53oSxCMH8al-h2ObLuhYf45QFrdimuDlCKJN_g/formResponse",
                 data: {
-                    name: name,
-                    email: email,
-                    message: message
+                    entry.833710143: name,
+                    entry.1376413320: email,
+                    entry.752045271: message
                 },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+				type: "POST",
+				dataType: "xml",
+				statusCode: {
+					0: function() {
+						// Success message
+						$('#success').html("<div class='alert alert-success'>");
+						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-success')
+							.append("<strong>Your message has been sent. </strong>");
+						$('#success > .alert-success')
+							.append('</div>');
 
-                    //clear all fields
-                    $('#gform').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", there was an error sending the e-mail. Please try again!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#gform').trigger("reset");
-                },
+						//clear all fields
+						$('#gform').trigger("reset");
+					},
+					200: function() {
+						// Fail message
+						$('#success').html("<div class='alert alert-danger'>");
+						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", there was an error sending the e-mail. Please try again!");
+						$('#success > .alert-danger').append('</div>');
+						//clear all fields
+						$('#gform').trigger("reset");
+					}
+				}
             });
-        },
+        }/*,
         filter: function() {
             return $(this).is(":visible");
         },
@@ -65,6 +68,6 @@ $(function() {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#entry.833710143').focus(function() {
     $('#success').html('');
 });
